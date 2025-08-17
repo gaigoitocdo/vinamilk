fac82327948f52967bb96730e71f5e5cefc83f9e
gaigoitocdo <lengocdanh4767@gmail.com>
2025-08-17 13:25:54 +0700

    Add files via upload

--- models/LotteryEditModel.php
+++ models/LotteryEditModel.php
@@ -2,137 +2,72 @@
 
 include_once __DIR__ . "/../config/config.php";
 include_once __DIR__ . "/../models/LotteryItemModel.php";
+include_once __DIR__ . "/../models/LotteryModel.php";
+include_once __DIR__ . "/../models/UserModel.php";
 include_once __DIR__ . "/../models/LotterySessionModel.php";
 
-class LotteryModel
+class LotteryEditModel
 {
-    public static function GetAllCategories()
+    public static function Get($key, $s)
     {
         $db = Database::getInstance();
-        $data = $db->pdo_query("SELECT vc.*, COUNT(v.id) AS lottery_num FROM `lottery_category` vc LEFT JOIN `lottery` v ON vc.id = v.cate_id GROUP BY vc.id;");
-        return $data;
+        return $db->pdo_query_one("SELECT * FROM `lottery_edit` WHERE `key` = '$key' AND `session` = '$s'");
     }
 
-    public static function CreateCategory($data)
+    public static function GetAll($key)
     {
-        $data["create_time"] = time();
-        $data["update_time"] = time();
+        $v = LotteryModel::GetByKey($key);
 
-        $db = Database::getInstance();
-        return $db->insert("lottery_category", $data);
-    }
+        if (!$v) json_response(500, ["success" => false, "message" => "Cannot get information"]);
 
-    public static function GetCategory($id)
-    {
-        $db = Database::getInstance();
-        $data = $db->pdo_query_one("SELECT * FROM `lottery_category` WHERE `id` = '$id'");
-        return $data;
-    }
+        $second = $v['rule'] * 60;
+        $now_time = (date('H') * 60 * 60 + date('i') * 60);
+        $now_expect = date('Ymd') . $v['rule'] . intval($now_time / $second);
 
-    public static function DeleteCategory($id)
-    {
-        $db = Database::getInstance();
-        $data = $db->pdo_execute("DELETE FROM `lottery_category` WHERE `id` = '$id'");
-        return $data;
-    }
+        $now_info = LotterySessionModel::Get($v["id"], $now_expect);
+        if (!$now_info) {
+            $now_info = ['create_time' => time()];
+        }
 
-    public static function UpdateCategory($id, $data)
-    {
-        $cat = self::GetCategory($id);
+        $arr = [];
 
-        if (empty($cat)) json_response(400, ["success" => false, "message" => "Category $id not found"]);
+        for ($i = 0; $i < 50; $i++) {
+            $lottery_edit = self::Get($key, $now_expect + $i);
 
-        $d = [];
-        $d["name"] = $data["name"] != NULL ? $data["name"] : $cat['name'];
-        $d["status"] = $data["status"] != NULL ? $data["status"] : $cat['status'];
-        $d["order"] = $data["order"] != NULL ? $data["order"] : $cat['order'];
-
-        $db = Database::getInstance();
-        $data = $db->update("lottery_category", $d, $id);
-        return $data;
-    }
-
-    public static function GetAll()
-    {
-        $db = Database::getInstance();
-        $data = $db->pdo_query("SELECT l.*, lc.name AS cate_name FROM `lottery` l INNER JOIN `lottery_category` lc ON l.cate_id = lc.id WHERE 1");
-        return $data;
-    }
-
-    public static function Get($id)
-    {
-        $db = Database::getInstance();
-        $data = $db->pdo_query_one("SELECT * FROM `lottery` WHERE `id` = '$id'");
-
-        if ($data) {
-            $s = LotterySessionModel::GetOneLatestByLID($data["id"]);
-            $data["items"] = [];
-            $data["prev_session"] = LotterySessionModel::GetPrevSessionID($data["rule"]);
-
-            if ($s && !empty($s["result"])) {//
-                $data["prev_session"] = $s["sid"];
-                $data["items"] = explode(",", $s["result"]);
+            $next_time = (date('H') * 60 * 60 + date('i') * 60);
+            if ($next_time + $second >= 86400) {
+                $next_time = 86400 - $next_time + $second;
+                $next_expect = date('Ymd') . $v['rule'] . intval($next_time / ($second * $i));
+            } else {
+                $next_expect = date('Ymd') . $v['rule'] . intval(($next_time + $second * $i) / $second);
             }
-            //
-            $data["now_session"] = LotterySessionModel::GetCurrentSessionID($data["rule"]);
-            $data["next_session"] = LotterySessionModel::GetNextSessionID($data["rule"]);
-            $data["second"] = LotterySessionModel::GetRemainSeconds($data["rule"]);
+
+            if (!$lottery_edit) {
+                // ✅ NEW: Generate random 5-digit code instead of A,B,C,D
+                $random_code = str_pad(rand(10000, 99999), 5, '0', STR_PAD_LEFT);
+
+                $arr[] = [
+                    'name' => $v['name'],
+                    'key' => $v['key'],
+                    'result' => $random_code, // 5-digit code instead of "A,D"
+                    'session' => $next_expect,
+                    'create_time' => $now_info['create_time'] + $second * $i,
+                ];
+            } else {
+                $u = UserModel::GetOne($lottery_edit["editor"]);
+                $arr[] = [
+                    'name' => $v['name'],
+                    'key' => $v['key'],
+                    'result' => $lottery_edit["result"], // Should be 5-digit code
+                    'editor' => $u ? $u["username"] : "unknown",
+                    'session' => $next_expect,
+                    'create_time' => $now_info['create_time'] + $second * $i,
+                    'edit_create_time' => $lottery_edit['create_time'],
+                    'edit_update_time' => $lottery_edit['update_time'],
+                ];
+            }
         }
 
-        return $data;
-    }
-
-    public static function GetByKey($key)
-    {
-        $db = Database::getInstance();
-        $data = $db->pdo_query_one("SELECT * FROM `lottery` WHERE `key` = '$key'");
-        return $data;
-    }
-
-    public static function Create($data)
-    {
-        if (!empty(self::GetByKey($data["key"])))
-            return json_response(400, ["success" => false, "message" => "Lottery key already exist, choose another key"]);
-
-        $db = Database::getInstance();
-        $id = $db->insert("lottery", $data);
-        if ($id) {
-            LotteryItemModel::Register($id, 1.1);
-        }
-    }
-
-    public static function Update($id, $data)
-    {
-        $l = self::Get($id);
-
-        if (empty($l)) json_response(400, ["success" => false, "message" => "Lottery $id not found"]);
-
-        $d = [];
-        $d["name"] = $data["name"] != NULL ? $data["name"] : $l['name'];
-        $d["status"] = $data["status"] != NULL ? $data["status"] : $l['status'];
-        $d["cate_id"] = $data["cate_id"] != NULL ? $data["cate_id"] : $l['cate_id'];
-        $d["desc"] = $data["desc"] != NULL ? $data["desc"] : $l['desc'];
-        $d["image"] = $data["image"] != NULL ? $data["image"] : $l['image'];
-        $d["rule"] = $data["rule"] != NULL ? $data["rule"] : $l['rule'];
-        $d["condition"] = $data["condition"] != NULL ? $data["condition"] : $l['condition'];
-        $d["hot"] = $data["hot"] != NULL ? $data["hot"] : $l['hot'];
-
-        $db = Database::getInstance();
-        $data = $db->update("lottery", $d, $id);
-        return $data;
-    }
-
-    public static function Delete($id)
-    {
-        $db = Database::getInstance();
-        $data = $db->pdo_execute("DELETE FROM `lottery` WHERE `id` = '$id'");
-        return $data;
-    }
-
-    public static function GetOdd($lid)
-    {
-        $db = Database::getInstance();
-        $data = $db->pdo_query("SELECT * FROM `lottery_item` WHERE `lid` = '$lid'");
-        return $data;
+        echo json_response(200, ["success" => true, "data" => $arr]);
     }
 }
